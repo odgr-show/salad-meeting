319c65c98eaa8e38970c6073f154b56109d776c2
odgr-show <121861788+odgr-show@users.noreply.github.com>
2024-01-21 00:47:07 +0900

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+import '../styles/style.scss';
+// import javascriptLogo from './javascript.svg';
+// import viteLogo from '/vite.svg';
+// import { setupCounter } from './counter.js';
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { eleventyPlugin } from 'vite-plugin-eleventy';
+
+export default {
+    root: 'src',
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+    },
+    plugins: [eleventyPlugin()],
+};
